38688f8bfd366cf65de314462fd869e622bbe3a6
churskir <churskir@ee.pw.edu.pl>
2018-12-05 10:52:25 +0100

    Implementacja wczytywania obrazów

--- src/main/java/FileManager.java
+++ src/main/java/FileManager.java
@@ -1,11 +1,40 @@
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class FileManager {
-    public BufferedImage readImage(String filePath) {
+
+    private String dirPath;
+    private String fileName;
+
+    public FileManager(String dirPath) {
+        this.dirPath = dirPath;
+    }
+
+    public BufferedImage readImage(String fileName) {
+        this.fileName = fileName;
+        try {
+            return ImageIO.read(new File(dirPath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public void saveImage(BufferedImage bufferedImage) {
+        try {
+            ImageIO.write(bufferedImage, getExtension(), new File(dirPath + "new_" + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private String getExtension() {
+        int i = fileName.lastIndexOf(".");
+        if (i == 0) {
+            return "";
+        }
+        return fileName.substring(i + 1);
     }
 }
--- src/test/java/FileManagerTest.java
+++ src/test/java/FileManagerTest.java
@@ -4,30 +4,34 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 public class FileManagerTest {
 
-    private String filePath1 = "src/test/resources/test_image_1.png";
+    private String dirPath = "src/test/resources/";
+    private String fileName1 = "test_image_1.png";
 
     @Test
     public void testReadImage() {
-        FileManager fileManager = new FileManager();
-        BufferedImage bufferedImage = fileManager.readImage(filePath1);
+        FileManager fileManager = new FileManager(dirPath);
+        BufferedImage bufferedImage = fileManager.readImage(fileName1);
         assert(bufferedImage != null);
     }
 
     @Test
     public void testWriteImage() {
-        FileManager fileManager = new FileManager();
-        fileManager.readImage(filePath1);
-        String expectedFilePath = "src/test/resources/new_test_image_1.png";
-        String filePath2 = "src/test/resources/test_image_2.png";
+        FileManager fileManager = new FileManager(dirPath);
+        fileManager.readImage(fileName1);
+        String expectedFilePath = dirPath + "new_test_image_1.png";
+        String filePath = dirPath + "test_image_2.png";
 
         try {
-            BufferedImage bufferedImage = ImageIO.read(new File(filePath2));
+            BufferedImage bufferedImage = ImageIO.read(new File(filePath));
             fileManager.saveImage(bufferedImage);
-            assert(new File(expectedFilePath).exists());
-            assert(ImageIO.read(new File(filePath2)).equals(ImageIO.read(new File(expectedFilePath))));
+            File expectedFile = new File(expectedFilePath);
+            expectedFile.deleteOnExit();
+            assert(expectedFile.exists());
         }
         catch (IOException e) {
             System.out.println("Exception in test");
